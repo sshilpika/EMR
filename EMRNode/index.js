aedd7176c3caefadd3b5791ea019324692e9e854
Shilpika S <sshilpika5@gmail.com>
2016-03-12 11:09:10 -0500

    adding first tab details and pre conditions(not working)

--- EMRNode/index.js
+++ EMRNode/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var databaseResults = require('./lib/connection.js')
 var app = express();
-var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' ,helpers: {section: function(name, options){ if(!this._sections) this._sections = {}; this._sections[name] = options.fn(this); return null;} }});
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
@@ -36,19 +36,40 @@ app.post('/say_hello', function (req, res) {
     });
   connection.query('CALL findPatientsByPhysicianId("?")',[55555], function(err, rows, fields) {
     if (!err){
-      console.log('1 ',rows[0]);
+      //console.log('1 ',rows[0]);
       res.render('patient-table',{result: rows[0]});
-      console.log('2 ',rows[0][0].SSN);
+      //console.log('2 ',rows[0][0].SSN);
     }else
       console.log(err);
     });
 
-  connection.end();
+  //connection.end();
 
 })
 
 app.get('/patient-details',function(req,res){
-  res.sendFile( __dirname + "/" + "login.html" );
+
+    var demog_query = 'SELECT First_Name, Last_Name, Gender, Birth_Date, SSN, Home_Address, City, State, ZipCode, Home_Phone FROM Person WHERE SSN = ?'
+    var pre_query = 'SELECT patient.EntryDate, pre.Value FROM PreExistingConditions pre, PatientsPreConditions patient WHERE patient.P_SSN = ? AND pre.Id = patient.PreConditionId ORDER BY patient.EntryDate DESC';
+  connection.query(demog_query,[req.query.ssn], function(err, rows, fields) {
+    if (!err){
+      console.log('1 ',rows[0]);
+      res.render('patient-demographics',{layout:'patient-info' ,result:rows[0]});
+     console.log('2 ',rows[0].First_Name);
+    }else
+      console.log(err);
+    });
+    /*connection.query(pre_query,[req.query.ssn], function(err, rows, fields) {
+      if (!err){
+        console.log('1 ',rows[0]);
+        res.render('patient-preconditions',{layout:null ,result:rows[0]});
+       //console.log('2 ',rows[0].Value);
+      }else
+        console.log(err);
+      });*/
+
+  connection.end();
+
 })
 
 app.get('/login.html',function(req, res){
