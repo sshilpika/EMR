7851cdb6f2492d8994004672bb58330f75e8f489
Shilpika S <sshilpika5@gmail.com>
2016-04-14 03:07:47 -0500

    visits section completed

---
+++ EMRNode/css/js/visits.js
@@ -0,0 +1,66 @@
+
+$(document).on('click','tr.visit-date',function(){
+   var dateSelectedVisit = this.textContent.trim();
+
+   var ssn = $('#ssn').text();
+   populateVisit(dateSelectedVisit);
+
+});
+function populateVisit(dateSelectedVisit){
+  var $deferredVisits = $.getJSON('visitsByDate?date='+dateSelectedVisit);
+
+  $.when($deferredVisits).done(function(response){
+      //clear visit by date
+      console.log(response+"!!!!!!!!!")
+      $("td.visit-by-date").empty();
+      //get travelNotes
+      var $travelNotes = response;
+      //process travelNotes array
+      console.log('TRVL NOTES '+JSON.stringify($travelNotes));
+      //$("td.visit-by-date").load("css/html-templates/visitFormToDisplay.html", function(){
+        $travelNotes.forEach(function(item) {
+          if (item !== null) {
+            var note = item;
+            console.log(JSON.stringify(item));
+            var visitId = item.Visit_ID;
+            var docName = item.First_Name + item.Last_Name;
+            var billNum = item.Bill_Num;
+            var comments = item.CommentsSuggestions;
+            var complaint = item.Complaint;
+            var $deferredDiagnosis = $.getJSON('getDiagnosis?visitId='+visitId);
+            var $deferredPrescriptions = $.getJSON('getPrescription?visitId='+visitId);
+
+            $.when($deferredDiagnosis,$deferredPrescriptions).done(function(resD, resP){
+              var $diagnosis = resD;
+              var $prescription = resP;
+              console.log('D '+JSON.stringify($diagnosis));
+              console.log('P '+JSON.stringify($prescription));
+              $("td.visit-by-date").load("css/html-templates/visitFormToDisplay.html", function(){
+                $diagnosis[0].forEach(function(diag){
+                  var li = $("<li>");
+                  li.html(diag.Diagnosis_Category);
+                  $('#diagnosisList').append(li);
+                });// end of diagnosis
+
+                $prescription[0].forEach(function(pres){
+                  var tr = $("<tr>");
+                  var td = $("<td>"), td1 = $("<td>")
+                  td.html(pres.Mname); td1.html(pres.Medicine_Quantity);
+                  tr.append([td,td1]);
+
+                  $('#prescription').append(tr);
+                });// end of prescription
+                $("#visitId").append(visitId);
+                $("#docName").append(docName);
+                $("#billNum").append(billNum);
+                $("#commentsSuggestions").append(comments);
+                $("#complaint").append(complaint);
+              });//end of load
+            });
+
+          }
+        });//end of forEach
+      //});//end of load
+
+    });
+}
--- EMRNode/index.js
+++ EMRNode/index.js
@@ -1,11 +1,17 @@
-var express = require('express');
+var express         = require('express');
 var databaseResults = require('./lib/connection.js')
-var app = express();
-var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' ,helpers: {section: function(name, options){ if(!this._sections) this._sections = {}; this._sections[name] = options.fn(this); return null;} }});
-var mysql      = require('mysql');
+var app             = express();
+var handlebars      = require('express3-handlebars').create({ defaultLayout: 'main' ,helpers: {section: function(name, options){ if(!this._sections) this._sections = {}; this._sections[name] = options.fn(this); return null;} }});
+var mysql           = require('mysql');
+var mongoose        = require('mongoose');
+var bcrypt          = require('bcrypt-nodejs');
+
+//defining login page
+//var team
+
 var connection = mysql.createConnection({
   host     : '127.0.0.1',
-  port: 3306,
+  port     : 3306,
   user     : 'root',
   password : 'root0808',
   database : 'EMR'
@@ -13,6 +19,7 @@ var connection = mysql.createConnection({
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.use(require('body-parser')());
+app.use(require('connect-flash')());
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/physician/css", express.static(__dirname + '/physician/css'));
 app.use("/physician/js", express.static(__dirname + '/physician/js'));
@@ -20,12 +27,7 @@ app.use("/physician/tab-content", express.static(__dirname + '/physician/tab-con
 
 
 app.post('/say_hello', function (req, res) {
-  console.log(req.body.username+" "+req.body.password+" "+req.body.loginas )
-  /*var pass = databaseResults.getResults('SELECT password FROM '+req.body.loginas+' WHERE username=?',req.body.username);
-  console.log(pass);*/
-  //var patientByPhysician = databaseResults.getResults('CALL findPatientsByPhysicianId("?")',55555);
-  //console.log("OUTSIDE ",databaseResults.getResults('CALL findPatientsByPhysicianId("?")',55555));
-  //var data = { RowDataPacket: [{SSN: '99999',First_Name: 'John',Last_Name: 'Smith',Birth_Date: 'Fri Jul 09 1965 00:00:00 GMT-0400 (EDT)',Gender: 'M',Current_Status: 'Heart Problem' }]};
+  console.log(req.body.username+" "+req.body.password+" "+req.body.loginas );
 
   connection.connect(function(err){
     if(err){
@@ -34,23 +36,38 @@ app.post('/say_hello', function (req, res) {
           }
     console.log('Connection established');
     });
-  connection.query('SELECT person.SSN,'+
-                    		'person.First_Name, '+
-                    		'person.Last_Name, '+
-                    		'person.Birth_Date, '+
-                    		'person.Gender, '+
-                    		'patient.Current_Status '+
-                    	'FROM Person person, Patient patient '+
-                    	'WHERE patient.D_SSN =? AND patient.SSN = person.SSN',[55555], function(err, rows, fields) {
-    if (!err){
-      console.log('1 ',rows);
-      res.render('patient-table',{result: rows});
-      //console.log('2 ',rows[0][0].SSN);
-    }else
-      console.log(err);
-    });
 
-  //connection.end();
+
+  connection.query('SELECT password, SSN FROM '+req.body.loginas+' WHERE username =? ',[req.body.username],function(err, pass, fields){
+
+
+      if (!err){
+        if(typeof pass[0] != 'undefined' && bcrypt.compareSync(req.body.password, pass[0].password)){
+            console.log('Getting patient info for doctor...');
+            connection.query('SELECT person.SSN,'+
+                              		'person.First_Name, '+
+                              		'person.Last_Name, '+
+                              		'person.Birth_Date, '+
+                              		'person.Gender, '+
+                              		'patient.Current_Status '+
+                              	'FROM Person person, Patient patient '+
+                              	'WHERE patient.D_SSN =? AND patient.SSN = person.SSN',[pass[0].SSN], function(err1, rows, fields) {
+              if (!err1){
+                console.log('1 ',rows);
+                res.render('patient-table',{result: rows});
+
+              }else
+                console.log(err1);
+              });
+          }else{
+            res.redirect('/login.html?e=' + encodeURIComponent('Incorrect username or password. Please try again.'));
+            console.log('Invalid details');
+          }
+    }else{
+      res.redirect('/login.html?e=' + encodeURIComponent('Incorrect username or password. Please try again.'));
+      console.log('Invalid details:'+err);
+    }
+  })
 
 })
 
@@ -188,6 +205,54 @@ app.get('/patient-visits',function(req,res){
   //connection.end();
 
 })
+
+app.get('/visitsByDate',function(req,res){
+    console.log('inside patient visit by date');
+
+    var allergy_query = "SELECT Visit_ID, physician.First_Name, physician.Last_Name, Bill_Num, CommentsSuggestions, Complaint FROM Visit visit, Person physician WHERE Date_Time = ? AND visit.D_SSN = physician.SSN";
+    var date = new Date(req.query.date);
+    console.log('VVVVV'+date);
+    connection.query(allergy_query,[date], function(err, rows, fields) {
+      if (!err){
+        console.log('1 ',rows);
+        res.json(rows);
+       console.log('2 ',rows[0]);
+      }else
+        console.log(err);
+      });
+})
+
+app.get('/getDiagnosis',function(req,res){
+    console.log('inside patient diagnosis by date');
+
+    var allergy_query = "SELECT Diagnosis_Category FROM Diagnosis WHERE Diagnosis_ID IN (SELECT Diagnosis_ID FROM Result WHERE Visit_ID =?)";
+
+    console.log('VVVVV'+req.query.visitId);
+    connection.query(allergy_query,[req.query.visitId], function(err, rows, fields) {
+      if (!err){
+        console.log('DIAGNOSIS ROWS ',rows);
+        res.json(rows);
+       console.log('2 ',rows[0]);
+      }else
+        console.log(err);
+      });
+})
+app.get('/getPrescription',function(req,res){
+    console.log('inside prescription visit by date');
+
+    var allergy_query = "SELECT Mname, Medicine_Quantity FROM Medicine med, Prescribed_Meds pres WHERE med.Minventory_ID = pres.Minventory_ID AND Prescription_ID = (SELECT Prescription_ID FROM Prescription WHERE Visit_ID =?)";
+
+    connection.query(allergy_query,[req.query.visitId], function(err, rows, fields) {
+      if (!err){
+        console.log('1 ',rows);
+        res.json(rows);
+       console.log('2 ',rows[0]);
+      }else
+        console.log(err);
+      });
+})
+
+
 app.get('/patient-prescriptions',function(req,res){
     console.log('inside patient allergies');
     var allergy_query = 'SELECT patient.EntryDate, allergies.Value FROM Allergies allergies, PatientsAllergies patient WHERE patient.P_SSN = ? AND allergies.Id = patient.AllergyId ORDER BY patient.EntryDate DESC';
